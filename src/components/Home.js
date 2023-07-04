850b82f032eaf147eca4685dec52e4525e48c9ca
Anand-P123 <anand1234job@gmail.com>
2023-07-04 20:02:08 +0530

    ecommerce

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ function Home() {
                <div className='cart-logo'>
                   <img src='https://www.freepnglogos.com/uploads/shopping-cart-png/shopping-cart-trolley-images-pixabay-download-pictures-14.png' alt="cart"></img>
                </div>
-            <h1>Home Component</h1>
+            <h1>E-commerce</h1>
             <div className="cart-wrapper">
                 <div className="img-wrapper item">
                     <img src="https://help.apple.com/assets/640A52196275DE31D4371B5E/640A52226275DE31D4371B8B/en_US/79a7175aaf9c5e2b255b0efa4f223ee5.png" style={{height:"150px"}}/>
