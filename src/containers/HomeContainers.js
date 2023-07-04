5245080d45ec15ec08726508763996f7f1998b5c
Anand-P123 <anand1234job@gmail.com>
2023-07-04 20:35:07 +0530

    add provider

--- src/containers/HomeContainers.js
+++ src/containers/HomeContainers.js
@@ -1,2 +1,12 @@
 import Home from "../components/Home";
-export default Home;
+import { connect } from "react-redux";
+import {addTocart} from "../Services/Actions/actions"
+
+const mapDispatchToProps=dispatch=>({
+
+})
+
+const mapStateToProps=state=>({
+
+})
+export default connect(mapDispatchToProps,mapStateToProps)(Home)
